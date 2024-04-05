0db801eceac6c0ad6406fdec312b7da9eb0cb773
Kun Ran <rankun203@gmail.com>
2024-04-05 20:52:31 +1100

    feat: first version

---
+++ README.md
@@ -0,0 +1,7 @@
+# HTML GPT
+
+## How to update
+
+1. Edit html-gpt.js and index.html
+2. For html-gpt.js, copy it to dist/, rename it to add DATE in the middle, upload to somewhere on the cloud
+3. For index.html, copy and paste into Shortcuts
---
+++ html-gpt.js
@@ -0,0 +1,72 @@
+const respDiv = document.getElementById("markdown-response");
+let cachedMd = null;
+
+const streamResults = async ({
+  systemPrompt,
+  query,
+  apiHost = "https://api.openai.com/v1/chat/completions",
+  model = "gpt-3.5-turbo",
+  apiKey,
+  respDiv,
+}) => {
+  const response = await fetch(apiHost, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: `Bearer ${apiKey}`,
+    },
+    body: JSON.stringify({
+      model,
+      messages: [
+        {
+          role: "system",
+          content: systemPrompt,
+        },
+        {
+          role: "user",
+          content: query,
+        },
+      ],
+      stream: true,
+    }),
+  });
+
+  const reader = response.body?.getReader();
+
+  let responseText = "";
+  while (reader && true) {
+    const { value, done } = await reader.read();
+    if (done) {
+      console.log("[streamResults] Stream complete");
+      break;
+    }
+    // Process each chunk
+    const chunk = new TextDecoder().decode(value);
+    chunk.split("\n").forEach((line) => {
+      if (line) {
+        line = line.replace("data: ", "");
+        if (!line.startsWith("{")) {
+          console.log("[streamResults] Ignoring line:", line);
+          return;
+        }
+        const json = JSON.parse(line);
+        console.log("[streamResults] Chunk:", json);
+        if ("content" in json.choices[0].delta) {
+          responseText += json.choices[0].delta.content;
+        }
+      }
+    });
+    if (!cachedMd) {
+      if ("markdownit" in window) {
+        cachedMd = window.markdownit();
+      }
+    }
+
+    if (cachedMd) {
+      const result = cachedMd.render(responseText);
+      respDiv.innerHTML = result;
+    } else {
+      respDiv.innerText = responseText;
+    }
+  }
+};
